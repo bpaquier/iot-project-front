500ac85f1686214b80137910c273081a8f21bbb1
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-07-19 12:01:55 +0200

    create component searchbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,14 @@ import css from "./styles.module.scss";
 const cx = classnames.bind(css);
 
 import BaseComponent from "~/components/@BaseComponent";
+import SearchBar from "~/components/SearchBar";
 
 export default function Home() {
   return (
     <div className={css.container}>
       <main className={css.main}>
         <BaseComponent />
+        <SearchBar />
       </main>
     </div>
   );
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,7 +1,23 @@
+import React, { useState } from 'react';
 import classnames from "classnames/bind";
 import css from "./styles.module.scss";
 const cx = classnames.bind(css);
+import Search from "~/components/Svgs/SearchIcon";
+
 
 export default function SearchBar() {
 
+  const [isActive, setActive] = useState(false);
+  const [searchValue, setIsSeachValue] = useState('');
+
+  const checkIsFocus = () => {
+    if(searchValue.length <= 0){
+      setActive(false)
+    }
+  }
+
+  return <div className={cx(css.searchBar, isActive ? css.red : null)} >
+    <Search/>
+    <input onChange={(e) => setIsSeachValue(e.target.value)} onBlur={checkIsFocus} onFocus={() => setActive(true)} className={css.input} type="text" name="" id="" />
+  </div>
 }
