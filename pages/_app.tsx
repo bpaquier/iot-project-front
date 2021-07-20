1c6f648ba7dcb3cb85cacdbbfb1345c83bb283a1
quentintrouve <trouve.quentin@gmail.com>
2021-07-20 23:31:02 +0200

    feat/open stage wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   const toggleListOfSpaces = (e) => {
     e.preventDefault();
     setToggleOpen(!toggleOpen);
-    console.log(toggleOpen);
   }
 
   return (
