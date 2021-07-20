fb4ce4e9736971c4e2691a0acf391a18de749890
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-07-20 16:52:28 +0200

    add breadcrumb component

---
+++ src/components/Svgs/arrow-right/index.tsx
@@ -0,0 +1,17 @@
+function arrow(){
+    return <svg
+    width={10}
+    height={14}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.02 7c0 .26-.094.497-.283.686l-6.15 5.913a.964.964 0 01-1.348-.024.964.964 0 01.024-1.348L6.703 7l-5.44-5.227C.884 1.418.884.803 1.239.425 1.594.046 2.209.046 2.587.4l6.15 5.913c.189.19.284.426.284.686z"
+      fill="#DFE1E6"
+    />
+  </svg>
+}
+
+export default arrow
