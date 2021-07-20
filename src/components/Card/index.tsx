50cbb8cf88999a3113cf1612ee31da1e722da296
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2021-07-20 23:56:27 +0200

    center content in card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,15 +2,24 @@ import classnames from "classnames/bind";
 import css from "./styles.module.scss";
 const cx = classnames.bind(css);
 
+type directionType = "column" | "row" | "column-reverse" | "row-reverse";
+interface IProps {
+  className?: string;
+  children?: React.ReactNode;
+  title?: string;
+  direction?: directionType;
+}
 
-function Card({className, children, title }) {
-
-  return <div 
-        className={cx(className, css.card)}
-        >
-           <h3>{title} </h3>
-      {children}
-  </div>
+function Card({ className, children, title, direction }): any {
+  return (
+    <div
+      className={cx(className, css.card)}
+      style={{ flexDirection: direction || "column" }}
+    >
+      <h3>{title} </h3>
+      <div className={css.contentContainer}>{children}</div>
+    </div>
+  );
 }
 
 export default Card;
