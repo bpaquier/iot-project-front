b2a0a9e8eb1cc9dbba69fc0120acea95533f254a
bpaquier <bastien.paquier@gmail.com>
2021-07-22 17:50:00 +0200

    add hook

--- src/hooks/useUpdatesPresence/index.js
+++ src/hooks/useUpdatesPresence/index.js
@@ -34,3 +34,38 @@ export function useUpdatedPresence() {
 
   return { isLoading, data };
 }
+
+export function useAveragePresence(start, stop) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    setIsLoading(true);
+
+    axios({
+      baseURL: "https://fluxeo-back.herokuapp.com",
+      url: `/presence-7-days/${start}/${stop}`,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((rep) => {
+        if (isMounted) {
+          setData(rep.data.data);
+          setIsLoading(false);
+        }
+      })
+      .catch((rep) => {
+        setData(rep);
+        setIsLoading(false);
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return { isLoading, data };
+}
