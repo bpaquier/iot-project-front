61f8ff362e52793adfd3dccfcbc871659f9d19b4
quentintrouve <trouve.quentin@gmail.com>
2021-07-22 10:07:05 +0200

    fix/ margin-left search icon

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -4,25 +4,24 @@ import css from "./styles.module.scss";
 const cx = classnames.bind(css);
 import Search from "~/components/Svgs/SearchIcon";
 
-
 export default function SearchBar() {
 
   const [isActive, setActive] = useState(false);
   const [searchValue, setIsSeachValue] = useState('');
 
   const checkIsFocus = () => {
-    if(searchValue.length <= 0){
+    if (searchValue.length <= 0) {
       setActive(false)
     }
   }
 
   return <div className={cx(css.searchBar, isActive ? css.red : null)} >
-    <Search/>
-    <input 
-      onChange={(e) => setIsSeachValue(e.target.value)} 
-      onBlur={checkIsFocus} onFocus={() => setActive(true)} 
-      className={css.input} 
-      type="text" 
-      placeholder="Rechercher par nom, équipe..."/>
+    <Search />
+    <input
+      onChange={(e) => setIsSeachValue(e.target.value)}
+      onBlur={checkIsFocus} onFocus={() => setActive(true)}
+      className={css.input}
+      type="text"
+      placeholder="Rechercher par nom, équipe..." />
   </div>
 }
